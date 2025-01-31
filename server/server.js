3eabdc03047126ef91d1b6e63f5da2b95ded44a8
AvishkarMandlik <mandlikavi121@gmail.com>
2025-01-31 07:22:48 +0530

    cors added

--- server/server.js
+++ server/server.js
@@ -14,7 +14,13 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 connectDB();
 
 app.use('/api/submissions', require('./routes/submissions'));
-
+app.use(
+    cors({
+      origin: "https://user-submission-system.onrender.com/api", 
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    })
+  );
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
